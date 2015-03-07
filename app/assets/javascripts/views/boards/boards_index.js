a012de4a1a6bf5e284b5de4adde7ea6355c7d0b2
Eric Hsu <erichsu7@gmail.com>
2015-03-06 17:22:51 -0800

    Style boards index

--- app/assets/javascripts/views/boards/boards_index.js
+++ app/assets/javascripts/views/boards/boards_index.js
@@ -3,6 +3,10 @@ TrelloClone.Views.BoardsIndex = Backbone.CompositeView.extend({
   template: JST["boards/boards_index"],
   className: "boards-index",
 
+  events: {
+    "click .boards-form-button": "showBoardsForm"
+  },
+
   initialize: function () {
     var that = this;
     this.listenTo(this.collection, "sync", this.render);
@@ -41,6 +45,12 @@ TrelloClone.Views.BoardsIndex = Backbone.CompositeView.extend({
         that.removeSubview(".boards-list", indexItemView);
       };
     });
+  },
+
+  showBoardsForm: function (event) {
+    this.$(".boards-form-button").toggle();
+    this.$(".boards-form").toggle();
+    this.$("#board_title").focus();
   }
 
 
