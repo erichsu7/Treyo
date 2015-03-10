179213a15b9608c7933cd1dd42ee7fdcf61735c5
Eric Hsu <erichsu7@gmail.com>
2015-03-09 22:37:14 -0700

    Remove delete board dropdown

--- app/assets/javascripts/views/boards/boards_index_item.js
+++ app/assets/javascripts/views/boards/boards_index_item.js
@@ -3,29 +3,10 @@ TrelloClone.Views.BoardsIndexItem = Backbone.CompositeView.extend({
   tagName: "li",
   className: "boards-index-item",
 
-  events: {
-    "click .delete": "toggleDropdown",
-  },
-
   render: function () {
     var renderedContent = this.template({ board: this.model });
     this.$el.html(renderedContent);
 
     return this;
   },
-
-  toggleDropdown: function () {
-    if (this.dropdown) {
-      this.dropdown.remove();
-      this.dropdown = null;
-      this.$(".delete").removeClass("fa fa-caret-square-o-up")
-      this.$(".delete").addClass("fa fa-caret-square-o-down");
-    } else {
-      this.dropdown = new TrelloClone.Views.BoardDropdown({ model: this.model });
-      this.addSubview(".delete-dropdown-container", this.dropdown);
-      this.$(".delete").removeClass("fa fa-caret-square-o-down");
-      this.$(".delete").addClass("fa fa-caret-square-o-up")
-    }
-  },
-
 });
